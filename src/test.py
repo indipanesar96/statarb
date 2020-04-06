858cba3d7fbd51365157e8dccd7fb58a9525b5a6
indipanesar96 <indipanesar96@gmail.com>
2020-04-06 23:58:48 +0100

    [IP] - removing non-production code

--- src/test.py
+++
@@ -1,7 +0,0 @@
-from src.DataRepository import DataRepository, DataLocations
-
-# all available features for ETFs
-self.current_window.repository.features[DataLocations.ETFs]
-
-# all available tickers for SNP
-self.current_window.repository.tickers[DataLocations.SNP]
